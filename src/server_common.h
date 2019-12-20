d6c6e8beae8baf9b9283e1d0e6b86ac7bad09413
frosty322 <frosty3221@mail.ru>
2019-12-20 18:34:31 +0300

    Update server_common.h

--- src/server_common.h
+++ src/server_common.h
@@ -6,14 +6,4 @@
 void init_server();
 void update_info(info* server_info);
 
-#endifs244720@helios:/home/s242480/spb/lab5/part1/test$ 
-s244720@helios:/home/s242480/spb/lab5/part1/test$ cat server_common.h
-#ifndef _SERVER_COMMON
-#define _SERVER_COMMON
-
-#include "server_info.h"
-
-void init_server();
-void update_info(info* server_info);
-
-#endifs
+#endif
